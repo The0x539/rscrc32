e8b9fd6e283544e2a61dfab8f57abe9996f5afc0
The0x539 <the0x539@gmail.com>
2022-11-11 15:39:44 -0600

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,109 @@
 use std::fs::File;
-use std::path::Path;
+use std::io::Result;
 
-use crc32fast::Hasher;
-use memmap::Mmap;
-use regex::Regex;
-use walkdir::WalkDir;
+use memmap2::Mmap;
+use rayon::prelude::*;
 
-fn hash<P: AsRef<Path>>(path: P) -> std::io::Result<u32> {
-    let f = File::open(path.as_ref())?;
-    let buf = unsafe { Mmap::map(&f)? };
+fn main() {
+    let paths = std::env::args().skip(1).collect::<Vec<_>>();
 
-    let mut hasher = Hasher::new();
-    hasher.update(&buf);
-    let checksum = hasher.finalize();
+    if paths.is_empty() {
+        match hash_stdin() {
+            Ok(crc) => println!("{crc:08X}"),
+            Err(error) => eprintln!("ERR {error}"),
+        }
+    } else if paths.len() == 1 {
+        let path = &paths[0];
+        let result = hash_file(path);
+        report_result(path, result, false);
+    } else {
+        let mut inputs = Vec::with_capacity(paths.len());
+        let mut outputs = Vec::with_capacity(paths.len());
 
-    Ok(checksum)
-}
+        for path in paths {
+            let (tx, rx) = oneshot::channel();
+            inputs.push((path, tx));
+            outputs.push(rx);
+        }
 
-fn check_file<T: AsRef<str>>(path: T, should_print_name: bool) -> std::io::Result<()> {
-    let path = path.as_ref();
+        // in the background, hash the files in no particular order...
+        std::thread::spawn(move || {
+            inputs.into_par_iter().for_each(|(path, tx)| {
+                let result = hash_file(&path);
+                let _ = tx.send((path, result));
+            });
+        });
 
-    let sum = hash(path)?;
-    print!("{:08X}", sum);
-
-    if should_print_name {
-        print!("\t{}", path);
-    }
-
-    const PAT: &str = "[[:^xdigit:]]([[:xdigit:]]{8})[[:^xdigit:]]";
-    let re = Regex::new(PAT).unwrap();
-
-    if let Some(caps) = re.captures(path) {
-        let filename_sum = u32::from_str_radix(&caps[1], 16).unwrap();
-        if sum == filename_sum {
-            print!(" \tOK");
-        } else {
-            print!(" \tBAD {:08X} != {:08X}", sum, filename_sum);
+        // ...but report the results in the order the files were listed
+        for rx in outputs {
+            let (path, result) = rx.recv().unwrap();
+            report_result(&path, result, true);
         }
     }
-
-    println!();
-    Ok(())
 }
 
-fn check_item(path: &str, multiple_args: bool) -> std::io::Result<()> {
-    let metadata = std::fs::metadata(path)?; // folllows symlinks
-    if metadata.is_file() {
-        check_file(path, multiple_args)?;
+fn hash_stdin() -> Result<u32> {
+    let stdin = std::io::stdin().lock();
+    let mmap = unsafe { Mmap::map(&stdin)? };
+
+    #[cfg(unix)]
+    mmap.advise(memmap2::Advice::Sequential)?;
+
+    Ok(crc32fast::hash(&mmap))
+}
+
+fn hash_file(path: &str) -> Result<u32> {
+    let f = File::open(path)?;
+    let mmap = unsafe { Mmap::map(&f)? };
+
+    #[cfg(unix)]
+    mmap.advise(memmap2::Advice::Sequential)?;
+
+    Ok(crc32fast::hash(&mmap))
+}
+
+fn report_result(path: &str, result: Result<u32>, print_path: bool) {
+    if let Ok(crc) = result {
+        print!("{crc:08X}");
     } else {
-        assert!(metadata.is_dir());
-        for entry in WalkDir::new(path)
-            .follow_links(true)
-            .into_iter()
-            .filter_map(|e| e.ok())
-        {
-            if let Ok(metadata) = entry.metadata() {
-                if metadata.is_file() {
-                    let path = entry.path().to_str().expect("invalid unicode in path");
-                    check_file(path, true)?;
+        print!("????????");
+    }
+
+    if print_path {
+        print!("\t{path}")
+    }
+
+    match result {
+        Ok(crc) => {
+            if let Some(name_crc) = find_crc_in_name(path) {
+                if crc == name_crc {
+                    print!("\tOK");
+                } else {
+                    print!("\tBAD {crc:08X} != {name_crc:08X}");
                 }
             }
         }
+        Err(error) => print!("\tERR {error}"),
     }
-    Ok(())
+
+    println!();
 }
 
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().skip(1).collect();
+fn find_crc_in_name(filepath: &str) -> Option<u32> {
+    filepath.as_bytes().windows(10).rev().find_map(parse_crc)
+}
 
-    for arg in &args {
-        check_item(arg, args.len() > 1)?;
+fn parse_crc(window: &[u8]) -> Option<u32> {
+    if window.len() != 10 {
+        return None;
     }
 
-    Ok(())
+    if window[0].is_ascii_hexdigit() || window[9].is_ascii_hexdigit() {
+        return None;
+    }
+
+    let s = std::str::from_utf8(&window[1..9]).ok()?;
+    let crc = u32::from_str_radix(s, 16).ok()?;
+
+    Some(crc)
 }
